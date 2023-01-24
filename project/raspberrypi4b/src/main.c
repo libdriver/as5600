c0d0fe5a9f97fde3a1d4d2b163a55a0a416a8275
e <lishifenging@outlook.com>
2023-01-24 18:20:12 +0800

    style: change the code style

--- project/raspberrypi4b/src/main.c
+++ project/raspberrypi4b/src/main.c
@@ -45,9 +45,9 @@
  * @param[in] argc is arg numbers
  * @param[in] **argv is the arg address
  * @return    status code
- *             - 0 success
- *             - 1 run failed
- *             - 5 param is invalid
+ *            - 0 success
+ *            - 1 run failed
+ *            - 5 param is invalid
  * @note      none
  */
 uint8_t as5600(uint8_t argc, char **argv)
